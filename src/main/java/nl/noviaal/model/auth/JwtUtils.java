c6fd27eeedda55f0c84309c4712337a5c40ca807
Jurjen Vorhauer <jurjen.vorhauer@jdriven.com>
2021-07-06 15:38:31 +0200

    added username to notes response.

--- src/main/java/nl/noviaal/model/auth/JwtUtils.java
+++ src/main/java/nl/noviaal/model/auth/JwtUtils.java
@@ -8,7 +8,8 @@ import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.security.Keys;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
@@ -17,9 +18,9 @@ import java.security.Key;
 import java.util.Date;
 
 @Component
-@Slf4j
 public class JwtUtils {
 
+  private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
   private static final Key KEY = Keys.secretKeyFor(SignatureAlgorithm.HS512);
 
   @Value("${noviaal.jwt.expiration.ms}")
@@ -54,15 +55,15 @@ public class JwtUtils {
         parseJwtToken(token);
         return true;
       } catch (SecurityException e) {
-        log.error("Invalid JWT security: {}", e.getMessage());
+        logger.error("Invalid JWT security: {}", e.getMessage());
       } catch (MalformedJwtException e) {
-        log.error("Invalid JWT token: {}", e.getMessage());
+        logger.error("Invalid JWT token: {}", e.getMessage());
       } catch (ExpiredJwtException e) {
-        log.error("JWT token is expired: {}", e.getMessage());
+        logger.error("JWT token is expired: {}", e.getMessage());
       } catch (UnsupportedJwtException e) {
-        log.error("JWT token is unsupported: {}", e.getMessage());
+        logger.error("JWT token is unsupported: {}", e.getMessage());
       } catch (IllegalArgumentException e) {
-        log.error("JWT claims string is empty: {}", e.getMessage());
+        logger.error("JWT claims string is empty: {}", e.getMessage());
       }
     }
     return false;
--- src/main/java/nl/noviaal/model/response/ItemResponse.java
+++ src/main/java/nl/noviaal/model/response/ItemResponse.java
@@ -7,111 +7,114 @@ import nl.noviaal.domain.Media;
 import nl.noviaal.domain.Note;
 import nl.noviaal.domain.Tag;
 
-import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 import java.util.stream.Collectors;
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class ItemResponse {
 
-  private final static DateTimeFormatter DTF = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private final static DateTimeFormatter DTF = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-  private final String id;
-  private final String created;
-  private final String type;
-  private final List<CommentResponse> comments = new ArrayList<>();
-  private String tags;
+    private final String id;
+    private final String created;
+    private final String type;
+    private final String username;
+    private final List<CommentResponse> comments = new ArrayList<>();
+    private String tags;
 
-  // Note content, leave null or empty to not include in JSON output
-  private String title;
-  private String body;
+    // Note content, leave null or empty to not include in JSON output
+    private String title;
+    private String body;
 
-  // Media content, leave null or blank to not include in JSON output
-  private String name;
-  private String contentType;
+    // Media content, leave null or blank to not include in JSON output
+    private String name;
+    private String contentType;
 
 
-  public ItemResponse(String id, String created, String type) {
-    this.id = id;
-    this.created = created;
-    this.type = type;
-  }
-
-  public String getId() {
-    return id;
-  }
-
-  public String getCreated() {
-    return created;
-  }
-
-  public String getType() {
-    return type;
-  }
-
-  public String getTitle() {
-    return title;
-  }
-
-  public String getBody() {
-    return body;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public String getContentType() {
-    return contentType;
-  }
-
-  public void setTitle(String title) {
-    this.title = title;
-  }
-
-  public void setBody(String body) {
-    this.body = body;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public void addComment(Comment comment) {
-    comments.add(CommentResponse.ofComment(comment));
-  }
-
-  public List<CommentResponse> getComments() {
-    return comments;
-  }
-
-  public void setContentType(String contentType) {
-    this.contentType = contentType;
-  }
-
-  public static ItemResponse ofItem(Item item) {
-    final ItemResponse itemResponse = new ItemResponse(
-      item.getId().toString(),
-      item.getCreated().format(DTF),
-      item.getClass().getSimpleName()
-    );
-    for (Comment comment : item.getComments()) {
-      itemResponse.addComment(comment);
+    public ItemResponse(String id, String created, String type, String username) {
+        this.id = id;
+        this.created = created;
+        this.type = type;
+        this.username = username;
     }
-    itemResponse.tags = item.getTags().stream().map(Tag::getName).collect(Collectors.joining(", "));
-    if (item instanceof Note) {
-      Note note = (Note) item;
-      itemResponse.setTitle(note.getTitle());
-      itemResponse.setBody(note.getBody());
+
+    public String getId() {
+        return id;
     }
-    if (item instanceof Media) {
-      Media media = (Media) item;
-      itemResponse.setName(media.getName());
-      itemResponse.setContentType(media.getContentType());
+
+    public String getCreated() {
+        return created;
     }
-    return itemResponse;
-  }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getUsername() {
+      return username;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void addComment(Comment comment) {
+        comments.add(CommentResponse.ofComment(comment));
+    }
+
+    public List<CommentResponse> getComments() {
+        return comments;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public static ItemResponse ofItem(Item item) {
+        final ItemResponse itemResponse = new ItemResponse(
+                item.getId().toString(),
+                item.getCreated().format(DTF),
+                item.getClass().getSimpleName(),
+                item.getAuthor().getName()
+        );
+        for (Comment comment : item.getComments()) {
+            itemResponse.addComment(comment);
+        }
+        itemResponse.tags = item.getTags().stream().map(Tag::getName).collect(Collectors.joining(", "));
+        if (item instanceof Note note) {
+            itemResponse.setTitle(note.getTitle());
+            itemResponse.setBody(note.getBody());
+        }
+        if (item instanceof Media media) {
+            itemResponse.setName(media.getName());
+            itemResponse.setContentType(media.getContentType());
+        }
+        return itemResponse;
+    }
 }
